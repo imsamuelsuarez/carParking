4c3cef198eae9b12155e49b08e7fcbeddd25bf5a
Samuel <samuel.suarez@wimbu.co>
2023-04-23 22:37:45 -0400

    Basic project structure. A single route done

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,19 @@
 <script>
-  import Hello from './lib/helllo.svelte'
+  
+  import Pernoctas from './routes/(private)/pernoctas/+page.svelte'
 
-  class pernocta{
-    constructor(brand, model, owner){
-        this.brand = brand,
-        this.model = model,
-        this.owner = owner
-    }
-}
 
-const pernoctas = []
 
-// pernoctas.push(new pernocta('ford', 'fiesta', "luis"))
-// pernoctas.push(new pernocta('toyota', 'corolla', 'samuel'))
 
-console.log(pernoctas)
+
 
 
 </script>
 
-<main class="flex justify-center">
 
-  <section class="w-11/12 h-screen">
-    <table class="flex relative w-full min-h-1/2 max-h-auto flex-col border-gray-1px text-center">
-      <tr>
-        <header class="flex justify-evenly">
-          <th>Marca</th>
-          <th>Modelo</th>
-          <th>Propietario</th>
-        </header>
-      </tr>
-      {#if !pernoctas.length}
-          <h1 class="absolute top-1/2 left-">No hay pernoctas registradas!</h1>
-      {/if}
-      {#each pernoctas as pernocta}
-      <tr class="flex justify-evenly">
-        <td>{pernocta.brand}</td>
-        <td>{pernocta.model}</td>
-        <td>{pernocta.owner}</td>
-      </tr>
-      {/each}
-    </table> 
-  </section>
-  
+<main>
+
+
 </main>
 
 <style>
